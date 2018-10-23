e23760a5f4dd9b3d93e91055959191435dc11124
user <user@DESKTOP-HOT4PQK>
2018-10-23 15:13:10 +0900

    del

--- Ecada/src/main/java/com/tj/dao/VideoDao.java
+++
@@ -1,9 +0,0 @@
-package com.tj.dao;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public interface VideoDao {
-
-	public ArrayList<HashMap<String, Object>> list(HashMap<String, String> params);
-}
--- Ecada/src/main/java/com/tj/service/VideoService.java
+++
@@ -1,9 +0,0 @@
-package com.tj.service;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public interface VideoService {
-
-	public ArrayList<HashMap<String, Object>> list(HashMap<String, String> params);
-}
